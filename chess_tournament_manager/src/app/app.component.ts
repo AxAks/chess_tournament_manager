6d962aeb76319a865fa279827cfd49beb2da87bc
Aksel <axel.akonde@gmail.com>
2022-08-22 10:44:38 +0200

    bah 2nd commit ...

--- chess_tournament_manager/src/app/app.component.ts
+++ chess_tournament_manager/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chess_tournament_manager';
+export class AppComponent implements OnInit {
+  title!: string;
+  api!: string;
+
+  ngOnInit() {
+    this.title = 'Chess Tournament Manager';
+    this.api = environment.ChessManagerApi
+  };
+
+  async fetchPlayersList(): Promise<any> {
+    let response = await fetch(this.api + 'players/', {
+      method: 'GET'
+    });
+    let playersList = await response.json()
+    return playersList
+  };
 }
