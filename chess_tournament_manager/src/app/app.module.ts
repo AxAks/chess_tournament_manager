6b37a4c042f5a4c34c1e888084d39141dce94773
Aksel <axel.akonde@gmail.com>
2022-08-29 00:02:59 +0200

    traduction et locales, français par défaut

--- chess_tournament_manager/src/app/app.module.ts
+++ chess_tournament_manager/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -18,7 +18,7 @@ import { TournamentsListComponent } from './home/tournaments-list/tournaments-li
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
